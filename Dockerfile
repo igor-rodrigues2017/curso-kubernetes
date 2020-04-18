5dc01fb041a42c2239840608621b6bfe11691629
Igor Rodrigues <igor.chagas.rodrigues@gmail.com>
2020-04-18 00:56:54 -0300

    projeto final do curso

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM jnlucas/image-phpmysql:latest
+
+#Utilizado para a aplicação
+#COPY noticias /var/www/html
+
+#Utilizado para o sistema
+COPY sistema /var/www/html
---
+++ sistema/enviarMensagem.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Olá, um novo pod foi criado!"}' https://hooks.slack.com/services/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
